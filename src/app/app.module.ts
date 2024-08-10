b54206a4abea8486dd73b9f41e275b845e7b08ee
rudrasantadip <santadip9@gmail.com>
2024-08-11 00:28:15 +0530

    project1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VideoBackgroundComponent } from './video-bg/video-background/video-background.component';
+import { NavigationModule } from './navigation/navigation.module';
+import { HomeModule } from './home/home.module';
+import { EventModule } from './event/event.module';
+import { ContactModule } from './contact/contact.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VideoBackgroundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
+    // Custom modules
+    NavigationModule,
+    HomeModule,
+    EventModule,
+    ContactModule
   ],
   providers: [],
   bootstrap: [AppComponent]
